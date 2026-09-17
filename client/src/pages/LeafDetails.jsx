8f56799a8d45bb938503ae24b8672fa8a3e693c4
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-09-17 23:40:49 +0530

    feat: handle invalid leaf id

--- client/src/pages/LeafDetails.jsx
+++ client/src/pages/LeafDetails.jsx
@@ -1,12 +1,22 @@
 import { useParams } from 'react-router-dom'
+import { leaves } from '../data'
 
 function LeafDetails() {
   const { id } = useParams()
 
+  const selectedLeaf = leaves.find((leaf) => leaf.id === id)
+
+  if (!selectedLeaf) {
+    return <p>Leaf not found</p>
+  }
+
   return (
     <section>
       <h2>Leaf Details</h2>
-      <p>Selected leaf: {id}</p>
+      <p>{selectedLeaf.leafType}</p>
+      <p>{selectedLeaf.quantity}</p>
+      <p>{selectedLeaf.location}</p>
+      <p>{selectedLeaf.description}</p>
     </section>
   )
 }
