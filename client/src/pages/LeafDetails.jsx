63541e31ba796bcbdaa1c9512e0dc1b9e71bba67
Ramesh Raveendran <rameshraveendranm@gmail.com>
2026-09-17 23:46:35 +0530

    feat: add back navigation

--- client/src/pages/LeafDetails.jsx
+++ client/src/pages/LeafDetails.jsx
@@ -1,13 +1,15 @@
-import { useParams } from 'react-router-dom'
-import { leaves } from '../data'
+import { useParams } from "react-router-dom";
+import { leaves } from "../data";
+import { useNavigate } from "react-router-dom";
 
 function LeafDetails() {
-  const { id } = useParams()
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const selectedLeaf = leaves.find((leaf) => leaf.id === id)
+  const selectedLeaf = leaves.find((leaf) => leaf.id === id);
 
   if (!selectedLeaf) {
-    return <p>Leaf not found</p>
+    return <p>Leaf not found</p>;
   }
 
   return (
@@ -17,8 +19,11 @@ function LeafDetails() {
       <p>{selectedLeaf.quantity}</p>
       <p>{selectedLeaf.location}</p>
       <p>{selectedLeaf.description}</p>
+      <button type="button" onClick={() => navigate("/")}>
+        Back to Leaves
+      </button>
     </section>
-  )
+  );
 }
 
-export default LeafDetails
+export default LeafDetails;
